76d0282f841c4d2c00f8d26597fd8566fdd6b9c4
Yekateryna Boiko <yekateryna.boyko@gmail.com>
2026-03-30 23:08:09 +0300

    add new functionality to class Geometry

--- Geometry.cs
+++ Geometry.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +21,24 @@ namespace Geometry
                Y = vector1.Y + vector2.Y
             };
             
+        }
+
+
+        public static double GetLength(Segment segment)
+        {
+            double segmentX = segment.End.X - segment.Begin.X;
+            double segmentY = segment.End.Y - segment.Begin.Y;
+            return Math.Sqrt(segmentX * segmentX + segmentY * segmentY);
+        }
+
+        public static bool IsVectorSegment(Vector v, Segment s)
+        {
+            double length = GetLength(s);
+            double lengthBegin = Math.Sqrt(Math.Pow(v.X - s.Begin.X, 2) + Math.Pow(v.Y - s.Begin.Y, 2));
+            double lengthEnd = Math.Sqrt(Math.Pow(v.X - s.End.X, 2) + Math.Pow(v.Y - s.End.Y, 2));
+            double sumOfLegth = lengthBegin + lengthEnd;
+
+            return Math.Abs((lengthBegin + lengthEnd) - length) < 1e-8;
         }
     }
 }
